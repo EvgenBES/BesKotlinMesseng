7243b6712d824e471d952e0c2095d6e19d71ea5b
EvgenBES <40202338+EvgenBES@users.noreply.github.com>
2018-11-19 21:10:45 +0300

    changed ChatLogActivity, created chat (saved and log database)

--- app/src/main/java/com/example/fox/beskotlinmasseng/messages/LatestMessagesActivity.kt
+++ app/src/main/java/com/example/fox/beskotlinmasseng/messages/LatestMessagesActivity.kt
@@ -6,19 +6,42 @@ import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import com.example.fox.beskotlinmasseng.R
+import com.example.fox.beskotlinmasseng.model.User
 import com.example.fox.beskotlinmasseng.registerlogin.RegisterActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class LatestMessagesActivity : AppCompatActivity() {
+
+    companion object {
+        var currentUser: User? = null
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_latest_messages)
 
-
+        fetchCurrentUser()
 
         verifyUserIsLoggedIn()
 
+    }
+
+    private fun fetchCurrentUser() {
+        val uid = FirebaseAuth.getInstance().uid
+        val ref = FirebaseDatabase.getInstance().getReference("/users/$uid")
+        ref.addListenerForSingleValueEvent(object: ValueEventListener{
+            override fun onDataChange(p0: DataSnapshot) {
+                currentUser = p0.getValue(User::class.java)
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
     }
 
     private fun verifyUserIsLoggedIn() {
